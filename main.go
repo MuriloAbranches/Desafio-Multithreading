7f11b02d4cace09c5fd5492ea3eaaf719c10e5ad
MuriloAbranches <muriloads@hotmail.com>
2023-01-30 19:26:32 -0300

    Added response structs for each type of request

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -11,18 +13,48 @@ const (
 	VIA_CEP_URL = "http://viacep.com.br/ws/"
 )
 
+type ApiCepResponse struct {
+	Code       string `json:"code"`
+	State      string `json:"state"`
+	City       string `json:"city"`
+	District   string `json:"district"`
+	Address    string `json:"address"`
+	Status     int    `json:"status"`
+	Ok         bool   `json:"ok"`
+	StatusText string `json:"statusText"`
+}
+
+type ViaCepResponse struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
 func main() {
-	channelApiCep := make(chan string)
-	channelViaCep := make(chan string)
+	channelApiCep := make(chan ApiCepResponse)
+	channelViaCep := make(chan ViaCepResponse)
 
 	cep := "01310-000"
 
 	go func() {
 		url := API_CEP_URL + cep + ".json"
-
+		
 		response := request(url)
 		if response != nil {
-			channelApiCep <- string(response)
+			var apiCepResponse ApiCepResponse
+
+			if err := json.Unmarshal(response, &apiCepResponse); err != nil {
+				return
+			}
+
+			channelApiCep <- apiCepResponse
 		}
 	}()
 
@@ -31,19 +63,25 @@ func main() {
 
 		response := request(url)
 		if response != nil {
-			channelViaCep <- string(response)
+			var viaCepResponse ViaCepResponse
+
+			if err := json.Unmarshal(response, &viaCepResponse); err != nil {
+				return
+			}
+
+			channelViaCep <- viaCepResponse
 		}
 	}()
 
 	select {
 	case response := <-channelApiCep:
-		println(response)
+		fmt.Printf("API CEP Response: %+v\n", response)
 
 	case response := <-channelViaCep:
-		println(response)
+		fmt.Printf("Via CEP Response: %+v \n", response)
 
 	case <-time.After(time.Second):
-		println("Request timeout")
+		fmt.Println("Request timeout")
 	}
 }
 
